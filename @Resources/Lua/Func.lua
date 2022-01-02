87eab4730075937fd389518be94ce13ed58381e2
Jax <80020581+EnhancedJax@users.noreply.github.com>
2022-01-02 22:50:25 +0800

    v2.2

--- @Resources/Lua/Func.lua
+++ @Resources/Lua/Func.lua
@@ -1,15 +1,25 @@
 mediaPlayers = {'AIMP', 'CAD', 'WMP', 'iTunes', 'Winamp', 'WebNowPlaying'}
 
-function checkMedia()
-
-    if currentPlayer == nil then currentPlayer = SKIN:GetVariable('NowPlayingMedia') end
-    if checkingPlayer == nil then checkingPlayer = 'AIMP' end
+function checkMediaAuto()
+    currentPlayer = nil
     for i = 1, 6 do
-        if SKIN:GetMeasure('state'..mediaPlayers[i]):GetValue() ~= 0 then
-            checkingPlayer = mediaPlayers[i]
+        if SKIN:GetMeasure('state'..mediaPlayers[i]):GetValue() == 1 then
+            currentPlayer = mediaPlayers[i]
             break
         end
     end
+    if currentPlayer == nil then
+        for i = 1, 6 do
+            if SKIN:GetMeasure('state'..mediaPlayers[i]):GetValue() == 2 then
+                currentPlayer = mediaPlayers[i]
+                break
+            end
+        end
+    end
+    if currentPlayer == nil then currentPlayer = SKIN:GetVariable('NowPlayingMedia') end
+
+    checkingPlayer = currentPlayer
+
     checkingPlayerState = SKIN:GetMeasure('state'..checkingPlayer):GetValue()
 
     -- print(checkingPlayer, checkingPlayerState)
@@ -62,7 +72,52 @@ function checkMedia()
     SKIN:Bang('[!SetVariable NowPlayingMedia '..checkingPlayer..']')
     SKIN:Bang('[!UpdateMeasureGroup Music]')
 
-    currentPlayer = checkingPlayer
+    if tonumber(SKIN:GetVariable('Control')) == 1 then
+        if checkingPlayerState == 1 then SKIN:Bang('[!SetOption PlayPause MeterStyle "Pause"][!UpdateMeter PlayPause]')
+        elseif checkingPlayerState == 2 then SKIN:Bang('[!SetOption PlayPause MeterStyle "Play"][!UpdateMeter PlayPause]')
+        elseif checkingPlayerState == 0 then SKIN:Bang('[!SetOption PlayPause MeterStyle "Play"][!UpdateMeter PlayPause]')
+        end
+    end
+    
+    if tonumber(SKIN:GetVariable('AutoHide')) == 1 then
+        if checkingPlayerState ~= 0 then 
+            SKIN:Bang('[!ShowFade]')
+        else
+            SKIN:Bang('[!HideFade]')
+        end
+    end
+
+    if checkingPlayerState == 0 then
+        SKIN:Bang('[!HideMeterGroup Music][!UpdateMeterGroup Music]')
+    else
+        SKIN:Bang('[!ShowMeterGroup Music][!UpdateMeterGroup Music]')
+    end
+    
+    SKIN:Bang('[!Redraw]')
+end
+
+
+function checkMediaModern()
+
+    checkingPlayerState = SKIN:GetMeasure('stateModern'):GetValue()
+
+    if SKIN:GetVariable('Shuffle') == 0 then
+        SKIN:Bang('[!DisableMeasure modernShuffle]')
+    end
+
+    if SKIN:GetVariable('Repeat') == 0 then
+        SKIN:Bang('[!DisableMeasure modernRepeat]')
+    end
+
+    if SKIN:GetVariable('Heart') == 0 then
+        SKIN:Bang('[!DisableMeasure modernHeart]')
+    end
+
+    if SKIN:GetVariable('Prog') == 0 then
+        SKIN:Bang('[!DisableMeasure modernProgress][!DisableMeasure modernCurrentPosition][!DisableMeasure modernLength]')
+    end
+        
+    SKIN:Bang('[!UpdateMeasureGroup Music]')
 
     if tonumber(SKIN:GetVariable('Control')) == 1 then
         if checkingPlayerState == 1 then SKIN:Bang('[!SetOption PlayPause MeterStyle "Pause"][!UpdateMeter PlayPause]')
